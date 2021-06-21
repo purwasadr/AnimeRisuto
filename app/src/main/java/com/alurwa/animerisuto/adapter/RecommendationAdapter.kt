07c3a1aefc672b2d1101e966dcea135115941a4c
Purwa Sadr <purwasadr@gmail.com>
2021-06-21 15:41:27 +0700

    Add RecommendationAdapter

---
+++ app/src/main/java/com/alurwa/animerisuto/adapter/RecommendationAdapter.kt
@@ -0,0 +1,41 @@
+package com.alurwa.animerisuto.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.alurwa.animerisuto.databinding.ListItemRecommendationsBinding
+import com.alurwa.animerisuto.model.Anime
+
+/**
+ * Created by Purwa Shadr Al 'urwa on 21/06/2021
+ */
+
+class RecommendationAdapter(
+    private val animeList: List<Anime>
+) : RecyclerView.Adapter<RecommendationAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(
+            ListItemRecommendationsBinding.inflate(
+                LayoutInflater.from(parent.context),parent, false
+            )
+        )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount() = animeList.size
+
+
+    inner class ViewHolder(
+      private val binding: ListItemRecommendationsBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            
+        }
+    }
+
+
+}
