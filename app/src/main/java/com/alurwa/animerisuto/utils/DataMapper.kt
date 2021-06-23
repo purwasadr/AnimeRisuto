74b5946b259620d2ef3d5619372ffa21c506655b
Purwa Sadr <purwasadr@gmail.com>
2021-06-23 12:48:06 +0700

    Add mapping for animeDetailEntityToDomain animeDetailResponseToEntity

--- app/src/main/java/com/alurwa/animerisuto/utils/DataMapper.kt
+++ app/src/main/java/com/alurwa/animerisuto/utils/DataMapper.kt
@@ -1,9 +1,14 @@
 package com.alurwa.animerisuto.utils
 
+import com.alurwa.animerisuto.data.source.local.entity.AnimeDetailEntity
 import com.alurwa.animerisuto.data.source.local.entity.AnimeEntity
 import com.alurwa.animerisuto.data.source.local.entity.MangaEntity
+import com.alurwa.animerisuto.data.source.remote.response.AnimeDetailResponse
 import com.alurwa.animerisuto.data.source.remote.response.AnimeListResponse
 import com.alurwa.animerisuto.data.source.remote.response.MangaListResponse
+import com.alurwa.animerisuto.model.AnimeDetail
+import com.alurwa.animerisuto.model.AnimeRecommendation
+import com.alurwa.animerisuto.model.StartSeason
 
 /**
  * Created by Purwa Shadr Al 'urwa on 16/05/2021
@@ -45,23 +50,40 @@ object DataMapper {
             )
         }
 
-    /* fun animeResponseListToEntity2(input: AnimeSuggestions): List<AnimeEntity> =
-         input.data.map {
-             AnimeEntity(
-                 id = it.node.id,
-                 paging = input.paging.next,
-                 title = it.node.title,
-                 mainPicture = it.node.mainPicture?.let { mainPicture ->
-                     MainPicture(mainPicture.large, mainPicture.medium)
-                 },
-                 genres = it.node.genres.map { genre ->
-                     Genre(genre.id, genre.name)
-                 },
-                 mean = it.node.mean
-             )
-         }
+    fun animeDetailResponseToEntity(
+        input: AnimeDetailResponse
+    ): AnimeDetailEntity =
+        AnimeDetailEntity(
+            id = input.id,
+            title = input.title,
+            mainPictureUrl = input.mainPicture?.medium,
+            numEpisodes = input.numEpisodes,
+            startSeason = input.startSeason?.let {
+                StartSeason(it.year, it.season)
+            },
+            rank = input.rank,
+            mean = input.mean,
+            synopsis = input.synopsis,
+            recommendations = input.recommendations.map {
+                AnimeRecommendation(it.node.id, it.node.title, it.node.mainPicture?.medium)
+            }
 
-     */
+        )
 
-    // fun animeEntityListToDomain(input: List<AnimeEntity>): List<Anime> =
+    fun animeDetailEntityToDomain(
+        input: AnimeDetailEntity
+    ): AnimeDetail =
+        input.run {
+            AnimeDetail(
+                id = id,
+                title = title,
+                mainPictureUrl = mainPictureUrl,
+                numEpisodes = input.numEpisodes,
+                startSeason = input.startSeason,
+                rank = input.rank,
+                mean = mean,
+                synopsis = synopsis,
+                recommendations = recommendations
+            )
+        }
 }
