67448eb877c2b123768543e1bfecba45e8295a1e
Purwa Sadr <purwasadr@gmail.com>
2021-06-23 12:46:24 +0700

    Init for AnimeDetailActivity

---
+++ app/src/main/java/com/alurwa/animerisuto/model/AnimeRecommendation.kt
@@ -0,0 +1,11 @@
+package com.alurwa.animerisuto.model
+
+/**
+ * Created by Purwa Shadr Al 'urwa on 22/06/2021
+ */
+
+data class AnimeRecommendation(
+    val id: Int,
+    val title: String,
+    val mainPictureUrl: String?,
+)
