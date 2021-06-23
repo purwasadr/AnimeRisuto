fad7985966b8a9eef704f61ec8c67577555cb824
Purwa Sadr <purwasadr@gmail.com>
2021-06-23 12:37:43 +0700

    Add StartSeason and AnimeRecommendations TypeConverter

--- app/src/main/java/com/alurwa/animerisuto/data/source/local/room/Converters.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/source/local/room/Converters.kt
@@ -1,8 +1,10 @@
 package com.alurwa.animerisuto.data.source.local.room
 
 import androidx.room.TypeConverter
+import com.alurwa.animerisuto.model.AnimeRecommendation
 import com.alurwa.animerisuto.model.Genre
 import com.alurwa.animerisuto.model.MainPicture
+import com.alurwa.animerisuto.model.StartSeason
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
@@ -51,6 +53,34 @@ class Converters {
     fun toStrings(value: String): List<String> {
         val gson = Gson()
         val type = object : TypeToken<List<String>>() {}.type
+        return gson.fromJson(value, type)
+    }
+
+    @TypeConverter
+    fun fromStartSeason(value: StartSeason?): String? {
+        val gson = Gson()
+        val type = object : TypeToken<StartSeason?>() {}.type
+        return gson.toJson(value, type)
+    }
+
+    @TypeConverter
+    fun toStartSeason(value: String?): StartSeason? {
+        val gson = Gson()
+        val type = object : TypeToken<StartSeason?>() {}.type
+        return gson.fromJson(value, type)
+    }
+
+    @TypeConverter
+    fun fromAnimeRecommendations(value: List<AnimeRecommendation>): String {
+        val gson = Gson()
+        val type = object : TypeToken<List<AnimeRecommendation>>() {}.type
+        return gson.toJson(value, type)
+    }
+
+    @TypeConverter
+    fun toAnimeRecommendations(value: String): List<AnimeRecommendation> {
+        val gson = Gson()
+        val type = object : TypeToken<List<AnimeRecommendation>>() {}.type
         return gson.fromJson(value, type)
     }
 }
