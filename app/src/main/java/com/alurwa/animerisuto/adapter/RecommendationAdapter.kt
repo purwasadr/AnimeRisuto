2db64b966b1e675dbb9e7b5dc3b775d92d8e861c
Purwa Sadr <purwasadr@gmail.com>
2021-06-23 12:49:44 +0700

    Set data binding

--- app/src/main/java/com/alurwa/animerisuto/adapter/RecommendationAdapter.kt
+++ app/src/main/java/com/alurwa/animerisuto/adapter/RecommendationAdapter.kt
@@ -4,21 +4,20 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.alurwa.animerisuto.databinding.ListItemRecommendationsBinding
-import com.alurwa.animerisuto.model.Anime
+import com.alurwa.animerisuto.model.AnimeRecommendation
 
 /**
  * Created by Purwa Shadr Al 'urwa on 21/06/2021
  */
 
 class RecommendationAdapter(
-    private val animeList: List<Anime>
+    private val animeList: List<AnimeRecommendation>
 ) : RecyclerView.Adapter<RecommendationAdapter.ViewHolder>() {
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
         ViewHolder(
             ListItemRecommendationsBinding.inflate(
-                LayoutInflater.from(parent.context),parent, false
+                LayoutInflater.from(parent.context), parent, false
             )
         )
 
@@ -28,14 +27,13 @@ class RecommendationAdapter(
 
     override fun getItemCount() = animeList.size
 
-
     inner class ViewHolder(
-      private val binding: ListItemRecommendationsBinding
+        private val binding: ListItemRecommendationsBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
-            
+            val anime = animeList[position]
+
+            binding.animeRecommendation = anime
         }
     }
-
-
 }
