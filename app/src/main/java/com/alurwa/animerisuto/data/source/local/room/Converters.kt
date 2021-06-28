14d1dd83c5cf564014c011b7a2c91b8e7555ae8f
Purwa Sadr <purwasadr@gmail.com>
2021-06-28 10:03:40 +0700

    Add and remove converter

--- app/src/main/java/com/alurwa/animerisuto/data/source/local/room/Converters.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/source/local/room/Converters.kt
@@ -7,54 +7,13 @@ import com.alurwa.animerisuto.model.MainPicture
 import com.alurwa.animerisuto.model.StartSeason
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.util.StringJoiner
 
 /**
  * Created by Purwa Shadr Al 'urwa on 17/05/2021
  */
 
 class Converters {
-
-    @TypeConverter
-    fun fromGenres(value: List<Genre>): String {
-        val gson = Gson()
-        val type = object : TypeToken<List<Genre>>() {}.type
-        return gson.toJson(value, type)
-    }
-
-    @TypeConverter
-    fun toGenres(value: String): List<Genre> {
-        val gson = Gson()
-        val type = object : TypeToken<List<Genre>>() {}.type
-        return gson.fromJson(value, type)
-    }
-
-    @TypeConverter
-    fun fromMainPicture(value: MainPicture): String {
-        val gson = Gson()
-        val type = object : TypeToken<MainPicture>() {}.type
-        return gson.toJson(value, type)
-    }
-
-    @TypeConverter
-    fun toMainPicture(value: String): MainPicture {
-        val gson = Gson()
-        val type = object : TypeToken<MainPicture>() {}.type
-        return gson.fromJson(value, type)
-    }
-
-    @TypeConverter
-    fun fromStrings(value: List<String>): String {
-        val gson = Gson()
-        val type = object : TypeToken<List<String>>() {}.type
-        return gson.toJson(value, type)
-    }
-
-    @TypeConverter
-    fun toStrings(value: String): List<String> {
-        val gson = Gson()
-        val type = object : TypeToken<List<String>>() {}.type
-        return gson.fromJson(value, type)
-    }
 
     @TypeConverter
     fun fromStartSeason(value: StartSeason?): String? {
@@ -82,5 +41,15 @@ class Converters {
         val gson = Gson()
         val type = object : TypeToken<List<AnimeRecommendation>>() {}.type
         return gson.fromJson(value, type)
+    }
+
+    @TypeConverter
+    fun fromStringList(value: List<String>): String {
+        return value.joinToString(separator = ",")
+    }
+
+    @TypeConverter
+    fun toStringList(value: String): List<String> {
+        return value.split(',')
     }
 }
