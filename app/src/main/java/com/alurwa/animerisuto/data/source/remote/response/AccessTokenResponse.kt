771dca07d80e9e29592782ab366065e94cf8957b
Purwa Sadr <purwasadr@gmail.com>
2021-07-04 19:01:03 +0700

    Rename class because misspell

--- app/src/main/java/com/alurwa/animerisuto/data/source/remote/response/AccessTokenResponse.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/source/remote/response/AccessTokenResponse.kt
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName
  * Created by Purwa Shadr Al 'urwa on 04/05/2021
  */
 
-data class AccesTokenResponse(
+data class AccessTokenResponse(
     @field:SerializedName("token_type")
     val tokenType: String,
 
