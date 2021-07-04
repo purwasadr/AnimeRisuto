b80f4b79594ae236fa17c0ab37f9680695e097e3
Purwa Sadr <purwasadr@gmail.com>
2021-07-04 18:59:27 +0700

    Add function to get search result

--- app/src/main/java/com/alurwa/animerisuto/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/alurwa/animerisuto/ui/search/SearchViewModel.kt
@@ -1,8 +1,13 @@
 package com.alurwa.animerisuto.ui.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.alurwa.animerisuto.data.IAnimeRepository
+import com.alurwa.animerisuto.model.Anime
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 /**
@@ -14,4 +19,21 @@ class SearchViewModel @Inject constructor(
     private val repository: IAnimeRepository
 ) : ViewModel() {
 
+    private var currentQuery: String? = null
+    private var currentSearchResult: Flow<PagingData<Anime>>? = null
+
+    fun searchAnime(query: String): Flow<PagingData<Anime>> {
+        val lastResult = currentSearchResult
+        if (query == currentQuery && lastResult != null) {
+            return lastResult
+        }
+
+        currentQuery = query
+
+        val newResult = repository.getAnimeSearch(query).cachedIn(viewModelScope)
+
+        currentSearchResult = newResult
+
+        return newResult
+    }
 }
