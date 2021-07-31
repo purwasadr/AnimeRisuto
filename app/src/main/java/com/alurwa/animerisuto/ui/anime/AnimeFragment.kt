c43382d7791a0d2604b6412e733e06d7030d23a4
Purwa Sadr <purwasadr@gmail.com>
2021-07-31 11:31:59 +0700

    Add viewpager2 for navigation in each anime type

--- app/src/main/java/com/alurwa/animerisuto/ui/anime/AnimeFragment.kt
+++ app/src/main/java/com/alurwa/animerisuto/ui/anime/AnimeFragment.kt
@@ -1,28 +1,15 @@
 package com.alurwa.animerisuto.ui.anime
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import androidx.paging.LoadState
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.alurwa.animerisuto.adapter.AnimeAdapter
-import com.alurwa.animerisuto.adapter.AnimeLoadStateAdapter
+import com.alurwa.animerisuto.adapter.AnimePagerAdapter
 import com.alurwa.animerisuto.databinding.FragmentAnimeBinding
-import com.alurwa.animerisuto.ui.animedetail.AnimeDetailActivity
-import com.alurwa.animerisuto.utils.SpacingDecoration
-import com.alurwa.animerisuto.utils.asMergedLoadStates
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.distinctUntilChangedBy
-import kotlinx.coroutines.flow.filter
-import timber.log.Timber
 
 @AndroidEntryPoint
 class AnimeFragment : Fragment() {
@@ -32,10 +19,6 @@ class AnimeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: AnimeViewModel by viewModels()
-
-    private val adapter by lazy {
-        AnimeAdapter() { id -> navigateToAnimeDetail(id) }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,72 +32,33 @@ class AnimeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupAdapter()
 
-        setupRecyclerView()
+        viewModel
 
-        setupSwipeToRefresh()
-
-        getAnime()
+        setupViewPager()
+        setupTabLayoutMediator()
     }
 
-    private fun getAnime() {
-        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
-            viewModel.getAnimeList.collectLatest {
-                adapter.submitData(it)
+    private fun setupViewPager() {
+        binding.vp.adapter = AnimePagerAdapter(this)
+        binding.vp.offscreenPageLimit = 1
+        binding.vp.isUserInputEnabled = false
+    }
+
+    private fun setupTabLayoutMediator() {
+        TabLayoutMediator(
+            binding.tabLayout,
+            binding.vp,
+            true,
+            false
+        ) { tab, position ->
+            when (position) {
+                0 -> tab.text = "Suggestion"
+                1 -> tab.text = "Ranking"
+                2 -> tab.text = "Seasonal"
+                else -> throw IllegalStateException()
             }
-        }
-    }
-
-    private fun setupAdapter() {
-        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-            adapter.loadStateFlow.collectLatest { loadStates ->
-                binding.swipeRefresh.isRefreshing = loadStates.refresh is LoadState.Loading
-            }
-        }
-
-        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
-            adapter.loadStateFlow
-                .asMergedLoadStates()
-                // Only emit when REFRESH LoadState for RemoteMediator changes.
-                .distinctUntilChangedBy { it.refresh }
-                // Only react to cases where Remote REFRESH completes i.e., NotLoading.
-                .filter { it.refresh is LoadState.NotLoading }
-                .collect {
-                    Timber.d("Refresh tok " + it.refresh.toString())
-                    binding.rcvAnime.scrollToPosition(0)
-                }
-        }
-    }
-
-    private fun setupSwipeToRefresh() {
-        binding.swipeRefresh.setOnRefreshListener { adapter.refresh() }
-    }
-
-    private fun setupRecyclerView() {
-        binding.rcvAnime.setHasFixedSize(true)
-        binding.rcvAnime.layoutManager = LinearLayoutManager(requireContext())
-        binding.rcvAnime.addItemDecoration(
-            SpacingDecoration(
-                16,
-                16,
-                RecyclerView.VERTICAL
-            )
-        )
-        binding.rcvAnime.adapter = adapter.withLoadStateHeaderAndFooter(
-            header = AnimeLoadStateAdapter(),
-            footer = AnimeLoadStateAdapter() {
-                adapter.retry()
-            }
-        )
-    }
-
-    private fun navigateToAnimeDetail(id: Int) {
-        Intent(requireContext(), AnimeDetailActivity::class.java)
-            .putExtra(AnimeDetailActivity.EXTRA_ID, id)
-            .also {
-                startActivity(it)
-            }
+        }.attach()
     }
 
     override fun onDestroyView() {
