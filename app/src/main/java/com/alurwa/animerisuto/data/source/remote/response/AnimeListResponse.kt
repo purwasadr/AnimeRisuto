7d9bf5d895c8f46920f71ca20501027dcd24c061
Purwa Sadr <purwasadr@gmail.com>
2021-07-31 11:55:25 +0700

    Add extension function for mapping

--- app/src/main/java/com/alurwa/animerisuto/data/source/remote/response/AnimeListResponse.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/source/remote/response/AnimeListResponse.kt
@@ -1,5 +1,7 @@
 package com.alurwa.animerisuto.data.source.remote.response
 
+import com.alurwa.animerisuto.data.source.local.entity.AnimeGenreCrossRefEntity
+import com.alurwa.animerisuto.data.source.local.entity.GenreEntity
 import com.google.gson.annotations.SerializedName
 
 /**
@@ -11,3 +13,25 @@ data class AnimeListResponse(
     @field:SerializedName("node")
     val node: AnimeResponse
 )
+
+fun List<AnimeListResponse>.getGenreEntitySet(): Set<GenreEntity> {
+    val tempSet = mutableSetOf<GenreEntity>()
+    this.forEach {
+        it.node.genres?.forEach { genresRes ->
+            tempSet.add(GenreEntity(genresRes.id, genresRes.name))
+        }
+    }
+
+    return tempSet
+}
+
+fun List<AnimeListResponse>.getGenreCrossList(): List<AnimeGenreCrossRefEntity> {
+    val tempList = mutableListOf<AnimeGenreCrossRefEntity>()
+    this.forEach {
+        it.node.genres?.forEach { genresRes ->
+            tempList.add(AnimeGenreCrossRefEntity(it.node.id, genresRes.id))
+        }
+    }
+
+    return tempList
+}
