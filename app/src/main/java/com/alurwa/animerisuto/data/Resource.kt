990cb14eba3417f20a06b847e500733c841d600f
Purwa Sadr <purwasadr@gmail.com>
2021-07-31 11:32:44 +0700

    Remove comment

--- app/src/main/java/com/alurwa/animerisuto/data/Resource.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/Resource.kt
@@ -1,9 +1,5 @@
 package com.alurwa.animerisuto.data
 
-/**
- * Created by Purwa Shadr Al 'urwa on 08/05/2021
- */
-
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T) : Resource<T>(data)
     class Loading<T>(data: T? = null) : Resource<T>(data)
