abea583d49a4fa7820693e2ff36dda89ae8e9a5d
Purwa Sadr <purwasadr@gmail.com>
2021-07-31 11:29:25 +0700

    Add Ranking type enum

---
+++ app/src/main/java/com/alurwa/animerisuto/utils/AnimeRankingType.kt
@@ -0,0 +1,13 @@
+package com.alurwa.animerisuto.utils
+
+enum class AnimeRankingType(val code: String) {
+    ALL("all"),
+    AIRING("airing"),
+    UPCOMING("upcoming"),
+    TV("tv"),
+    OVA("ova"),
+    MOVIE("movie"),
+    SPECIAL("special"),
+    BYPOPULARITY("bypopularity"),
+    FAVORITE("favorite")
+}
