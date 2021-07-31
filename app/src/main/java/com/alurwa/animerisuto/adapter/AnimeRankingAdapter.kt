229b2cb316909ff4e90048682de39c1cbdd13223
Purwa Sadr <purwasadr@gmail.com>
2021-07-31 11:38:03 +0700

    Adapter for anime ranking

---
+++ app/src/main/java/com/alurwa/animerisuto/adapter/AnimeRankingAdapter.kt
@@ -0,0 +1,60 @@
+package com.alurwa.animerisuto.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.alurwa.animerisuto.databinding.ListItemAnimeRankingBinding
+import com.alurwa.animerisuto.model.Anime
+
+/**
+ * Created by Purwa Shadr Al 'urwa on 18/07/2021
+ */
+
+class AnimeRankingAdapter(
+    private val onItemClickCallback: (id: Int) -> Unit
+) : PagingDataAdapter<Anime, AnimeRankingAdapter.ViewHolder>(COMPARATOR) {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ListItemAnimeRankingBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    inner class ViewHolder(
+        private val binding: ListItemAnimeRankingBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            val item = getItem(position)
+
+            with(binding) {
+                anime = item
+                txtRank.text = (bindingAdapterPosition + 1).toString()
+                root.setOnClickListener {
+                    if (item != null) {
+                        onItemClickCallback.invoke(item.id)
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        val COMPARATOR = object : DiffUtil.ItemCallback<Anime>() {
+            override fun areContentsTheSame(oldItem: Anime, newItem: Anime): Boolean =
+                oldItem == newItem
+
+            override fun areItemsTheSame(oldItem: Anime, newItem: Anime): Boolean =
+                oldItem.id == newItem.id
+        }
+    }
+}
