50e44b6be8fb6ca59cfcdbee5b2038aedef61a64
Purwa Sadr <purwasadr@gmail.com>
2021-08-31 11:26:42 +0700

    Add repository for anime

---
+++ app/src/main/java/com/alurwa/animerisuto/data/repository/anime/AnimeRepository.kt
@@ -0,0 +1,81 @@
+package com.alurwa.animerisuto.data.repository.anime
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.alurwa.animerisuto.data.AnimePagingSource
+import com.alurwa.animerisuto.data.remotemediator.AnimeRankingRemoteMediator
+import com.alurwa.animerisuto.data.remotemediator.AnimeRemoteMediator3
+import com.alurwa.animerisuto.data.source.local.room.AnimeRisutoDatabase
+import com.alurwa.animerisuto.data.source.remote.network.ApiService
+import com.alurwa.animerisuto.model.Anime
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class AnimeRepository @Inject constructor(
+    private val service: ApiService,
+    private val database: AnimeRisutoDatabase
+) {
+    fun getAnimeSuggestions(): Flow<PagingData<Anime>> {
+        val pagingSourceFactory = { database.animeDao().getRemoteKeyAndAnime() }
+
+        @OptIn(ExperimentalPagingApi::class)
+        return Pager(
+            config = PagingConfig(pageSize = 10, enablePlaceholders = false),
+            remoteMediator = AnimeRemoteMediator3(
+                service,
+                database
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow.map {
+            it.map { anime ->
+                Anime(
+                    anime.animeEntity.id,
+                    anime.animeEntity.title,
+                    anime.animeEntity.posterPath,
+                    anime.animeEntity.genres,
+                    anime.animeEntity.mean
+                )
+            }
+        }
+    }
+
+    fun getAnimeRankingPaging(type: String): Flow<PagingData<Anime>> {
+        val pagingSourceFactory = {
+            database.animeRankingKeyDao().getRankingKeyAndAnime(type)
+        }
+
+        @OptIn(ExperimentalPagingApi::class)
+        return Pager(
+            config = PagingConfig(pageSize = 10, enablePlaceholders = false),
+            remoteMediator = AnimeRankingRemoteMediator(
+                type,
+                service,
+                database,
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow.map {
+            it.map { anime ->
+                Anime(
+                    anime.relation.id,
+                    anime.relation.title,
+                    anime.relation.posterPath,
+                    anime.relation.genres,
+                    anime.relation.mean
+                )
+            }
+        }
+    }
+
+    fun getAnimeSearch(query: String): Flow<PagingData<Anime>> =
+        Pager(
+            config = PagingConfig(
+                pageSize = 10,
+                enablePlaceholders = false,
+            ),
+            pagingSourceFactory = { AnimePagingSource(service, query) }
+        ).flow
+}
