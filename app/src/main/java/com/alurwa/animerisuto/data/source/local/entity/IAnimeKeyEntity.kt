b2703d882d4103c89cd3405ba8f8bf93eaba888f
Purwa Sadr <purwasadr@gmail.com>
2021-09-02 20:04:27 +0700

    Change doc

--- app/src/main/java/com/alurwa/animerisuto/data/source/local/entity/IAnimeKeyEntity.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/source/local/entity/IAnimeKeyEntity.kt
@@ -1,9 +1,8 @@
 package com.alurwa.animerisuto.data.source.local.entity
 
 /**
- * Created by Purwa Shadr Al 'urwa on 21/07/2021
+ * Digunakan untuk membuat data class Remote Key untuk RemoteMediator
  */
-
 interface IAnimeKeyEntity {
     val id: Long
     val animeId: Int
