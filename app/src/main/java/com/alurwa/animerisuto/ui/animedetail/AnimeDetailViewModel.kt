19e360096339ff8de44fce27c585193fb4bb026e
Purwa Sadr <purwasadr@gmail.com>
2021-09-02 20:54:12 +0700

    Change repository

--- app/src/main/java/com/alurwa/animerisuto/ui/animedetail/AnimeDetailViewModel.kt
+++ app/src/main/java/com/alurwa/animerisuto/ui/animedetail/AnimeDetailViewModel.kt
@@ -3,8 +3,16 @@ package com.alurwa.animerisuto.ui.animedetail
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.alurwa.animerisuto.data.IAnimeRepository
+import androidx.lifecycle.viewModelScope
+import com.alurwa.animerisuto.data.Resource
+import com.alurwa.animerisuto.data.repository.animedetail.AnimeDetailRepository
+import com.alurwa.animerisuto.model.AnimeDetail
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -13,10 +21,25 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AnimeDetailViewModel @Inject constructor(
-    private val repository: IAnimeRepository,
+    private val repository: AnimeDetailRepository,
     private val stateHandle: SavedStateHandle
 ) : ViewModel() {
     val extraId = stateHandle.get<Int>(AnimeDetailActivity.EXTRA_ID)!!
 
-    val animeDetail = repository.getAnimeDetails(extraId).asLiveData()
+    private var animeDetailJob: Job? = null
+
+    private var _animeDetail3: MutableStateFlow<Resource<AnimeDetail?>> =
+        MutableStateFlow(Resource.Loading())
+
+    val animeDetail3: StateFlow<Resource<AnimeDetail?>> = _animeDetail3
+
+    init {
+        animeDetailJob = viewModelScope.launch {
+            repository.getAnimeDetail(extraId).collectLatest {
+                _animeDetail3.value = it
+            }
+        }
+    }
+
+    val animeDetail = repository.getAnimeDetail(extraId).asLiveData()
 }
