43c0dcaf1c5bad5b6e3ec98eabde9612ba961f8b
Purwa Sadr <purwasadr@gmail.com>
2021-09-02 20:35:58 +0700

    Add mapper

--- app/src/main/java/com/alurwa/animerisuto/data/source/remote/response/AnimeListResponse.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/source/remote/response/AnimeListResponse.kt
@@ -1,7 +1,6 @@
 package com.alurwa.animerisuto.data.source.remote.response
 
-import com.alurwa.animerisuto.data.source.local.entity.AnimeGenreCrossRefEntity
-import com.alurwa.animerisuto.data.source.local.entity.GenreEntity
+import com.alurwa.animerisuto.data.source.local.entity.AnimeEntity
 import com.google.gson.annotations.SerializedName
 
 /**
@@ -13,6 +12,20 @@ data class AnimeListResponse(
     @field:SerializedName("node")
     val node: AnimeResponse
 )
+
+fun List<AnimeListResponse>.toEntityWithPaging(offset: Int): List<AnimeEntity> =
+    this.mapIndexed { index, it ->
+        AnimeEntity(
+            id = it.node.id,
+            no = offset + index,
+            title = it.node.title,
+            posterPath = it.node.mainPicture?.medium,
+            genres = it.node.genres?.map { genre ->
+                genre.name
+            },
+            mean = it.node.mean
+        )
+    }
 
 fun List<AnimeListResponse>.getGenreEntitySet(): Set<GenreEntity> {
     val tempSet = mutableSetOf<GenreEntity>()
