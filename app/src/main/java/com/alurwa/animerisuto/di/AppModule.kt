ef527f4f78b682242eb5e9e98c17957658e22d12
Purwa Sadr <purwasadr@gmail.com>
2021-09-02 20:50:59 +0700

    Add graph

--- app/src/main/java/com/alurwa/animerisuto/di/AppModule.kt
+++ app/src/main/java/com/alurwa/animerisuto/di/AppModule.kt
@@ -1,10 +1,16 @@
 package com.alurwa.animerisuto.di
 
+import android.content.Context
+import com.alurwa.animerisuto.constant.OAUTH_PREFERENCES
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 /**
@@ -15,7 +21,34 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @DispatchersIO
     @Singleton
     @Provides
     fun provideDispatchers() = Dispatchers.IO
+
+    @Provides
+    @OAuthSharedPreferences
+    fun provideOauthSharedPreferences(
+        @ApplicationContext context: Context
+    ) = context.getSharedPreferences(
+        OAUTH_PREFERENCES,
+        Context.MODE_PRIVATE
+    )
+
+    @ApplicationScope
+    @Provides
+    @Singleton
+    fun provideApplicationScope() = CoroutineScope(SupervisorJob())
 }
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class OAuthSharedPreferences
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class DispatchersIO
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApplicationScope
