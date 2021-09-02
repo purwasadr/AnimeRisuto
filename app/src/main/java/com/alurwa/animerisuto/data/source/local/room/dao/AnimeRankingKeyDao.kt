41cf6b3de7d5533b71ed4975265ef92cd283c352
Purwa Sadr <purwasadr@gmail.com>
2021-09-02 20:01:46 +0700

    Change AnimeRankingKeyDao interface to abstract class

--- app/src/main/java/com/alurwa/animerisuto/data/source/local/room/dao/AnimeRankingKeyDao.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/source/local/room/dao/AnimeRankingKeyDao.kt
@@ -14,18 +14,21 @@ import com.alurwa.animerisuto.data.source.local.resultentity.RankingKeysAndAnime
  */
 
 @Dao
-interface AnimeRankingKeyDao {
+abstract class AnimeRankingKeyDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(remoteKey: List<AnimeRankingKeyEntity>)
+    abstract suspend fun insertAll(remoteKey: List<AnimeRankingKeyEntity>)
 
     // `_rowid_`
     @Transaction
     @Query("SELECT * FROM ${AnimeRankingKeyEntity.TABLE_NAME} WHERE type = :type ORDER BY id ASC")
-    fun getRankingKeyAndAnime(type: String): PagingSource<Int, RankingKeysAndAnimeEntity>
+    abstract fun getRankingKeyAndAnime(type: String): PagingSource<Int, RankingKeysAndAnimeEntity>
 
     @Query("SELECT * FROM ${AnimeRankingKeyEntity.TABLE_NAME} WHERE anime_id = :animeId AND type = :type")
-    suspend fun remoteKeyId(type: String, animeId: Int): AnimeRankingKeyEntity?
+    abstract suspend fun remoteKeyId(type: String, animeId: Int): AnimeRankingKeyEntity?
 
     @Query("DELETE FROM ${AnimeRankingKeyEntity.TABLE_NAME} WHERE type = :type")
-    suspend fun clearRemoteKeys(type: String)
+    abstract suspend fun clearRemoteKeys(type: String)
+
+    @Transaction
+    open suspend fun withTransaction(tx: suspend () -> Unit) = tx()
 }
