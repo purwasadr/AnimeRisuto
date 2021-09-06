88f841de535fe77cbfe6482a163320cc96cca870
Purwa Sadr <purwasadr@gmail.com>
2021-09-06 11:46:42 +0700

    Add Season and Status Enum class

---
+++ app/src/main/java/com/alurwa/animerisuto/utils/SeasonType.kt
@@ -0,0 +1,8 @@
+package com.alurwa.animerisuto.utils
+
+enum class SeasonType(val code: String) {
+    WINTER("winter"),
+    SPRING("spring"),
+    SUMMER("summer"),
+    FALL("fall"),
+}
---
+++ app/src/main/java/com/alurwa/animerisuto/utils/StatusType.kt
@@ -0,0 +1,9 @@
+package com.alurwa.animerisuto.utils
+
+enum class StatusType(val code: String) {
+    WATCHING("watching"),
+    COMPLETED("completed"),
+    ON_HOLD("on_hold"),
+    DROPPED("dropped"),
+    PLAN_TO_WATCH("plan_to_watch"),
+}
