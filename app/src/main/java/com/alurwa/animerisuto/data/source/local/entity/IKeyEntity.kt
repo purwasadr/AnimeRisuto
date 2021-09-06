83e0fbb96905a2dffb60ea6c87891c0ab7491aba
Purwa Sadr <purwasadr@gmail.com>
2021-09-06 12:29:53 +0700

    change file name and remove animeId var

--- app/src/main/java/com/alurwa/animerisuto/data/source/local/entity/IKeyEntity.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/source/local/entity/IKeyEntity.kt
@@ -3,9 +3,8 @@ package com.alurwa.animerisuto.data.source.local.entity
 /**
  * Digunakan untuk membuat data class Remote Key untuk RemoteMediator
  */
-interface IAnimeKeyEntity {
+interface IKeyEntity {
     val id: Long
-    val animeId: Int
     val prevKey: Int?
     val nextKey: Int?
 }
