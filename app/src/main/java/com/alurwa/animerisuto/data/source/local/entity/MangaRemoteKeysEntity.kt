4610cf593f28fe3b16131390d6ecda07ee3d268d
Purwa Sadr <purwasadr@gmail.com>
2021-09-06 11:45:11 +0700

    Change table_name

--- app/src/main/java/com/alurwa/animerisuto/data/source/local/entity/MangaRemoteKeysEntity.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/source/local/entity/MangaRemoteKeysEntity.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
  * Created by Purwa Shadr Al 'urwa on 01/06/2021
  */
 
-@Entity(tableName = "manga_remote_keys")
+@Entity(tableName = "manga_remote_key")
 data class MangaRemoteKeysEntity(
     @PrimaryKey val id: Int,
     val prevKey: Int?,
