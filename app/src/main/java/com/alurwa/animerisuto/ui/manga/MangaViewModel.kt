5ab96aa65b975a5e2f45da7e84a094a1b79d3552
Purwa Sadr <purwasadr@gmail.com>
2021-09-06 11:43:23 +0700

    Change repository

--- app/src/main/java/com/alurwa/animerisuto/ui/manga/MangaViewModel.kt
+++ app/src/main/java/com/alurwa/animerisuto/ui/manga/MangaViewModel.kt
@@ -3,13 +3,13 @@ package com.alurwa.animerisuto.ui.manga
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
-import com.alurwa.animerisuto.data.IAnimeRepository
+import com.alurwa.animerisuto.data.repository.manga.MangaRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MangaViewModel @Inject constructor(
-    repository: IAnimeRepository
+    repository: MangaRepository
 ) : ViewModel() {
     val mangaPaging = repository.getMangaPaging().cachedIn(viewModelScope)
 }
