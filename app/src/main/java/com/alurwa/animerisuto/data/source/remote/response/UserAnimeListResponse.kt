2c02fa3e7404a5fe01724418976170ed92389843
Purwa Sadr <purwasadr@gmail.com>
2021-09-12 14:55:22 +0700

    Fix wrong attribute

--- app/src/main/java/com/alurwa/animerisuto/data/source/remote/response/UserAnimeListResponse.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/source/remote/response/UserAnimeListResponse.kt
@@ -3,34 +3,9 @@ package com.alurwa.animerisuto.data.source.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class UserAnimeListResponse(
+    @field:SerializedName("data")
+    val data: List<UserAnimeListPagingResponse>,
 
-	@field:SerializedName("is_rewatching")
-	val isRewatching: Boolean? = null,
-
-	@field:SerializedName("score")
-	val score: Int? = null,
-
-	@field:SerializedName("comments")
-	val comments: String? = null,
-
-	@field:SerializedName("updated_at")
-	val updatedAt: String? = null,
-
-	@field:SerializedName("rewatch_value")
-	val rewatchValue: Int? = null,
-
-	@field:SerializedName("priority")
-	val priority: Int? = null,
-
-	@field:SerializedName("num_times_rewatched")
-	val numTimesRewatched: Int? = null,
-
-	@field:SerializedName("num_watched_episodes")
-	val numWatchedEpisodes: Int? = null,
-
-	@field:SerializedName("status")
-	val status: String? = null,
-
-	@field:SerializedName("tags")
-	val tags: List<String>? = null
+    @field:SerializedName("paging")
+    val paging: PagingResponse
 )
