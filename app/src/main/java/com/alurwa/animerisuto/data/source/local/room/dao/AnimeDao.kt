fa3c6dfac867ba7ccb95caa1322ca06f95a778a2
Purwa Sadr <purwasadr@gmail.com>
2021-09-12 14:59:42 +0700

    Add insertOrUpdate function

--- app/src/main/java/com/alurwa/animerisuto/data/source/local/room/dao/AnimeDao.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/source/local/room/dao/AnimeDao.kt
@@ -6,6 +6,8 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
+import androidx.room.Update
+import com.alurwa.animerisuto.data.source.local.entity.AnimeDetailEntity
 import com.alurwa.animerisuto.data.source.local.entity.AnimeEntity
 import com.alurwa.animerisuto.data.source.local.resultentity.SuggestionKeysAndAnimeEntity
 
@@ -14,18 +16,30 @@ import com.alurwa.animerisuto.data.source.local.resultentity.SuggestionKeysAndAn
  */
 
 @Dao
-interface AnimeDao {
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(animeList: List<AnimeEntity>)
-
-    @Query("SELECT * FROM anime_list ORDER BY `no` ASC")
-    fun getAnimeList(): PagingSource<Int, AnimeEntity>
+abstract class AnimeDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE, entity = AnimeDetailEntity::class)
+    abstract suspend fun insert(entity: AnimeEntity): Long
 
     @Transaction
     @Query("SELECT * FROM anime_suggestion_remote_key ORDER BY `no` ASC")
-    fun getRemoteKeyAndAnime(): PagingSource<Int, SuggestionKeysAndAnimeEntity>
+    abstract fun getRemoteKeyAndAnime(): PagingSource<Int, SuggestionKeysAndAnimeEntity>
 
-    @Query("DELETE FROM anime_list")
-    suspend fun clearAnimeList()
+    @Query("DELETE FROM anime_detail")
+    abstract suspend fun clearAnimeList()
+
+    @Update(entity = AnimeDetailEntity::class)
+    abstract suspend fun update(entity: AnimeEntity)
+
+    open suspend fun insertOrUpdate(entity: AnimeEntity) {
+        if (insert(entity) == -1L) {
+            update(entity)
+        }
+    }
+
+    @Transaction
+    open suspend fun insertOrUpdate(entities: List<AnimeEntity>) {
+        entities.forEach {
+            insertOrUpdate(it)
+        }
+    }
 }
