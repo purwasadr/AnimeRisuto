e203f2010a0d23d9d5e08e751aa2f56c3e07aca3
Purwa Sadr <purwasadr@gmail.com>
2021-09-12 14:57:38 +0700

    Remove entity annotation

--- app/src/main/java/com/alurwa/animerisuto/data/source/local/entity/AnimeEntity.kt
+++ app/src/main/java/com/alurwa/animerisuto/data/source/local/entity/AnimeEntity.kt
@@ -1,29 +1,21 @@
 package com.alurwa.animerisuto.data.source.local.entity
 
 import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
 /**
  * Created by Purwa Shadr Al 'urwa on 15/05/2021
  */
 
-@Entity(tableName = "anime_list")
 data class AnimeEntity(
-
-    @PrimaryKey
     val id: Int,
 
-    val no: Int,
-    // var paging: Int = 0,
-
-    val title: String,
+    val title: String = "",
 
     // Poster path is fill from MainPicture.medium backend
-    @ColumnInfo(name = "poster_path")
-    val posterPath: String?,
+    @ColumnInfo(name = "main_picture_url")
+    val posterPath: String? = null,
 
-    val genres: List<String>?,
+    val genres: List<String>? = null,
 
-    val mean: Float?
+    val mean: Float? = null
 )
