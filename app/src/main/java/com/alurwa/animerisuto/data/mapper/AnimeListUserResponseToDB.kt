6b37ddb506f8e1121ee863264a0aae4466009f38
Purwa Sadr <purwasadr@gmail.com>
2021-09-12 14:33:25 +0700

    Add AnimeListUserResponseToDB and UserAnimeListResponseToDB mapper

---
+++ app/src/main/java/com/alurwa/animerisuto/data/mapper/AnimeListUserResponseToDB.kt
@@ -0,0 +1,25 @@
+package com.alurwa.animerisuto.data.mapper
+
+import com.alurwa.animerisuto.data.source.local.entity.AnimeListUserEntity
+import com.alurwa.animerisuto.data.source.remote.response.AnimeListStatusResponse
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AnimeListUserResponseToDB @Inject constructor() : Mapper<AnimeListStatusResponse, AnimeListUserEntity> {
+    override suspend fun map(from: AnimeListStatusResponse): AnimeListUserEntity =
+        from.run {
+            AnimeListUserEntity(
+                isRewatching = isRewatching,
+                score = score,
+                comments = comments.orEmpty(),
+                updatedAt = updatedAt.orEmpty(),
+                rewatchValue = rewatchValue,
+                priority = priority,
+                numTimesRewatched = numTimesRewatched,
+                numWatchedEpisodes = numWatchedEpisodes,
+                status = status.orEmpty(),
+                tags = tags
+            )
+        }
+}
---
+++ app/src/main/java/com/alurwa/animerisuto/data/mapper/UserAnimeListResponseToDB.kt
@@ -0,0 +1,24 @@
+package com.alurwa.animerisuto.data.mapper
+
+import com.alurwa.animerisuto.data.source.local.entity.UserAnimeListEntity
+import com.alurwa.animerisuto.data.source.remote.response.UserAnimeListPagingResponse
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UserAnimeListResponseToDB @Inject constructor(
+
+) : Mapper<UserAnimeListPagingResponse, UserAnimeListEntity> {
+    override suspend fun map(from: UserAnimeListPagingResponse): UserAnimeListEntity =
+        from.run {
+            UserAnimeListEntity(
+                id = node.id,
+                title = node.title,
+                mainPictureUrl = node.mainPicture?.medium.orEmpty(),
+                num_episode_watched = animeListStatus?.numWatchedEpisodes ?: 0 ,
+                numEpisodes = node.numEpisodes ?: 0,
+                list_status_score = animeListStatus?.score,
+                list_status_status = animeListStatus?.status.orEmpty()
+            )
+        }
+}
