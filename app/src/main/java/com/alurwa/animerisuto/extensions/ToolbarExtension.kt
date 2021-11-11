642f0eccb78fdac3f943d118a646960a3d9199a0
Purwa Sadr <purwasadr@gmail.com>
2021-11-11 21:49:53 +0700

    Add ToolbarExtension

---
+++ app/src/main/java/com/alurwa/animerisuto/extensions/ToolbarExtension.kt
@@ -0,0 +1,30 @@
+package com.alurwa.animerisuto.extensions
+
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+fun Toolbar.setupToolbar(
+    appCompat: AppCompatActivity,
+    title: String,
+    isHomeAsUpEnable: Boolean = false
+) {
+    with(appCompat) {
+        setSupportActionBar(this@setupToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(isHomeAsUpEnable)
+        supportActionBar?.setTitle(title)
+    }
+}
+
+fun Toolbar.setupToolbar(
+    appCompat: AppCompatActivity,
+    @StringRes
+    resTitle: Int,
+    isHomeAsUpEnable: Boolean = false
+) {
+    with(appCompat) {
+        setSupportActionBar(this@setupToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(isHomeAsUpEnable)
+        supportActionBar?.setTitle(resTitle)
+    }
+}
